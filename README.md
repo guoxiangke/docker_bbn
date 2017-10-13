c87c77c3071656e72d3956280987c4a5a7b67709
Your Name <you@example.com>
2017-10-13 12:18:22 +0800

    compose way

---
+++ README.md
@@ -0,0 +1,16 @@
+README.md
+
+remember:
+public-html/config.php
+
+<?php
+
+define('UPBUCKETNAME', '');
+define('UPOPNAME', '');
+define('UPOPPASS', '');
+define('CDNLINK', UPBUCKETNAME.'.b0.upaiyun.com');
+
+define('DEBUG', TRUE);
+define('ARCHIVE', 1);
+
+date_default_timezone_set('Asia/Shanghai');
--- run.sh
+++ run.sh
@@ -1,19 +1,4 @@
-# GitUrl="https://github.com/guoxiangke/lyaudio.git"
-
-# cd ./public-html
-# if [ -d .git ]; then
-#     sudo git checkout .
-#     sudo git fetch
-#     sudo git pull
-#   sudo git pull origin $GitBranch
-# else
-#   cd ../
-#   rm ./public-html -rf
-#   git clone $GitUrl ./public-html
-
-# fi;
-
-# cd ../
-docker build -t guoxiangke/docker_bbn  --no-cache  .
-docker rm -vf docker_bbn
-docker run -d -P --name docker_bbn  --restart=always guoxiangke/docker_bbn
+# docker build -t guoxiangke/docker_bbn  --no-cache  .
+# docker rm -vf docker_bbn
+# docker run -d -P --name docker_bbn  --restart=always guoxiangke/docker_bbn
+docker-compose -p bbn -f ./docker-compose.yml up -d  --build --remove-orphans --force-recreate
